acd3391c0ae3f6f37ba0d18f94bfb6f5e1abed13
AnjanaDelhi <anjanandelhi@gmail.com>
2020-12-11 17:49:39 -0600

    map layer

--- project/static/js/logic.js
+++ project/static/js/logic.js
@@ -4,6 +4,20 @@ var myMap = L.map("map", {
     zoom: 1,
   });
 
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+  
+  // Define a markerSize function that will give each city a different radius based on its population
+  function markerSize(happy) {
+    return happy;
+  }
+
 d3.json("/api/happy_map").then(function(wh_data, err) {
     if (err) throw err;
   
@@ -21,7 +35,7 @@ d3.json("/api/happy_map").then(function(wh_data, err) {
 //}
   
 // An array which will be used to store created cityMarkers
-var countryMarkers = [];
+//var countryMarkers = [];
 
 for (var i = 0; i < wh_data.length; i++) {
   L.circle([wh_data[i].latitude, wh_data[i].longitude], {
@@ -30,7 +44,7 @@ for (var i = 0; i < wh_data.length; i++) {
     fillColor: "purple",
     // Setting our circle's radius equal to the output of our markerSize function
     // This will make our marker's size proportionate to happiness
-    radius: wh_data[i].happy})
+    radius: markerSize(wh_data[i].happy)})
   
     // loop through the cities array, create a new marker, push it to the cityMarkers array
     //countryMarkers.push(
