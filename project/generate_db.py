8540cacb119df152c5bb901bb90238225afbd3b7
Dave Ruppel <drupps@gmail.com>
2020-12-10 19:02:31 -0800

    generate_db updates and copy of csv data

--- project/generate_db.py
+++ project/generate_db.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-wh20 = pd.read_csv("../data/tidy_data/WHR20_FinalData.csv")
+wh20 = pd.read_csv("../data/tidy_data/WHR20_columns_fixed.csv")
 engine = create_engine("sqlite:///wh20.sqlite")
 wh20.to_sql(name='wh20', con=engine, if_exists='replace', index=False)
