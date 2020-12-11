d804ed8730a4ebc0d45b9e6dc24ff35f6b11d5b3
AnjanaDelhi <anjanandelhi@gmail.com>
2020-12-11 17:01:33 -0600

    changes

--- project/static/js/logic.js
+++ project/static/js/logic.js
@@ -14,20 +14,33 @@ d3.json("/api/happy_map").then(function(wh_data, err) {
       data.happy = +data.happy;
       
     });
+
+// Define a markerSize function that will give each city a different radius based on its population
+//function markerSize(happy) {
+ // return happy;
+//}
   
 // An array which will be used to store created cityMarkers
 var countryMarkers = [];
 
 for (var i = 0; i < wh_data.length; i++) {
+  L.circle([wh_data[i].latitude, wh_data[i].longitude], {
+    fillOpacity: 0.75,
+    color: "red",
+    fillColor: "purple",
+    // Setting our circle's radius equal to the output of our markerSize function
+    // This will make our marker's size proportionate to happiness
+    radius: wh_data[i].happy})
+  
     // loop through the cities array, create a new marker, push it to the cityMarkers array
-    countryMarkers.push(
-      L.marker([wh_data[i].latitude, wh_data[i].longitude]).bindPopup("<h1>" + wh_data[i].country_name + "</h1>")
-    );
-  }
+    //countryMarkers.push(
+      L.marker([wh_data[i].latitude, wh_data[i].longitude]).bindPopup("<h1>" + wh_data[i].country_name + "<br>" + Math.round(wh_data[i].happy) +  "</h1>").addTo(myMap)
+    
+      };
 
-var countryLayer = L.layerGroup(countryMarkers);
+//var countryLayer = L.layerGroup(countryMarkers);
 
-countryLayer.addTo(myMap)
+//countryLayer.addTo(myMap)
 });
 
 
